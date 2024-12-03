343d2731487e9eb8a497f6dd616095c0fb036706
Joseneto Aguiar de Amorim <94873994+J0S3N3T0@users.noreply.github.com>
2024-12-03 19:01:51 -0300

    base url added

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -2,10 +2,6 @@ import userData from '../fixtures/userData.json'
 
 describe('Orange HRM Tests', () => {
 
-  const link ={
-    link: "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
-  }
-  
   const selectorsList ={
     usernameFiel: ":nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input",
     passwordFiel: ":nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input",
@@ -14,9 +10,9 @@ describe('Orange HRM Tests', () => {
     alertErrorLogin: ".oxd-alert",
     dashboardGrid: ".orangehrm-dashboard-grid > :nth-child(1)"
   }
-  
+
   it('Login - Sucess', () => {
-    cy.visit(link.link)
+    cy.visit('/auth/login')
     cy.get(selectorsList.usernameFiel).type(userData.userSucess.username)
     cy.get(selectorsList.passwordFiel).type(userData.userSucess.password)
     cy.get(selectorsList.loginButton).click()
@@ -25,14 +21,14 @@ describe('Orange HRM Tests', () => {
     cy.get(selectorsList.dashboardGrid)
   })
   it('Login - Fail - Wrong Username', () => {
-    cy.visit(link.link)
+    cy.visit('/auth/login')
     cy.get(selectorsList.usernameFiel).type(userData.userWrongName.username)
     cy.get(selectorsList.passwordFiel).type(userData.userWrongName.password)
     cy.get(selectorsList.loginButton).click()
     cy.get(selectorsList.alertErrorLogin)
   })
   it('Login - Fail - Wrong Password', () => {
-    cy.visit(link.link)
+    cy.visit('/auth/login')
     cy.get(selectorsList.usernameFiel).type(userData.userWrongPassword.username)
     cy.get(selectorsList.passwordFiel).type(userData.userWrongPassword.password)
     cy.get(selectorsList.loginButton).click()
