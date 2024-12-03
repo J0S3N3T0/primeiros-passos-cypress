8b551e9caf9a1148ae6f7b214b33bc2ddb74d218
Joseneto Aguiar de Amorim <94873994+J0S3N3T0@users.noreply.github.com>
2024-12-03 18:50:09 -0300

    Adding user data json

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,7 +1,12 @@
+import userData from '../fixtures/userData.json'
+
 describe('Orange HRM Tests', () => {
 
+  const link ={
+    link: "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+  }
+  
   const selectorsList ={
-    link: "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login",
     usernameFiel: ":nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input",
     passwordFiel: ":nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input",
     loginButton: ".oxd-button",
@@ -9,27 +14,27 @@ describe('Orange HRM Tests', () => {
     alertErrorLogin: ".oxd-alert",
     dashboardGrid: ".orangehrm-dashboard-grid > :nth-child(1)"
   }
-
+  
   it('Login - Sucess', () => {
-    cy.visit(selectorsList.link)
-    cy.get(selectorsList.usernameFiel).type('Admin')
-    cy.get(selectorsList.passwordFiel).type('admin123')
+    cy.visit(link.link)
+    cy.get(selectorsList.usernameFiel).type(userData.userSucess.username)
+    cy.get(selectorsList.passwordFiel).type(userData.userSucess.password)
     cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     //cy.get(selectorsList.selectionTitlleTopBar).contains('Dashboard')
     cy.get(selectorsList.dashboardGrid)
   })
-  it('Login - Fail - 1', () => {
-    cy.visit(selectorsList.link)
-    cy.get(selectorsList.usernameFiel).type('Admin1')
-    cy.get(selectorsList.passwordFiel).type('admin123')
+  it('Login - Fail - Wrong Username', () => {
+    cy.visit(link.link)
+    cy.get(selectorsList.usernameFiel).type(userData.userWrongName.username)
+    cy.get(selectorsList.passwordFiel).type(userData.userWrongName.password)
     cy.get(selectorsList.loginButton).click()
     cy.get(selectorsList.alertErrorLogin)
   })
-  it('Login - Fail - 2', () => {
-    cy.visit(selectorsList.link)
-    cy.get(selectorsList.usernameFiel).type('admin')
-    cy.get(selectorsList.passwordFiel).type('admin1234')
+  it('Login - Fail - Wrong Password', () => {
+    cy.visit(link.link)
+    cy.get(selectorsList.usernameFiel).type(userData.userWrongPassword.username)
+    cy.get(selectorsList.passwordFiel).type(userData.userWrongPassword.password)
     cy.get(selectorsList.loginButton).click()
     cy.get(selectorsList.alertErrorLogin)
   })
